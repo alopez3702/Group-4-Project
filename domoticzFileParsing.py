b8e4ba99cae824667d98f63b10a207bea445b2f7
gitForce99 <39219664+gitForce99@users.noreply.github.com>
2018-06-22 11:32:24 -0400

    Updating .json parsing
    Added some elif statements and a method to mark intrusion dates

--- domoticzFileParsing.py
+++ domoticzFileParsing.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jun 19 14:22:06 2018
 
-@author: maxjf, partially using Andrew's code from jsonToData.py
+@author: maxjf, partially using Andrew's code from jsonToData.py and parseiftttInProgress.py
 """
 
 import json
@@ -15,7 +15,7 @@ style.use('ggplot')
 from pandas.io.json import json_normalize
 import os
 
-#!!!!!!!!!!!!
+
 #read from multiple json files in a folder and parse the data
 data = [] #list to store data
 path = 'C:/Users/maxjf/Summer Internship 2018/domoticz Files' #make a folder and put all json files u want to use in it.
@@ -27,22 +27,22 @@ for infile in listing: #for every file in this folder
     if infile.endswith(".json"): #if it's a json file
         for line in open(infile): #read every line of the file
             data.append(json.loads(line)) #put every line into the list
-df = pd.DataFrame.from_dict(json_normalize(data), orient='columns') #put data from list into a DataFrame
+df_domoticz = pd.DataFrame.from_dict(json_normalize(data), orient='columns') #put data from list into a DataFrame
 #look on manipData2.py for other methods on how to change data in the DataFrame
 
 #choose certain columns
-df = df[['_id','_source.message']]
+df_domoticz = df_domoticz[['_id','_source.message']]
 
 #find the size of the column and store it
-#print(df.shape)
-size = df.shape
-numRows = size[0]
+#print(df_domoticz.shape)
+numRows = df_domoticz.shape[0]
+#numRows_intr = df_domoticz_intr.shape
 #print(numRows)
 uniqueMessageList = []
-#df.head(100)
+#df_domoticz.head(100)
 
 for x in range(numRows):
-    message = df.iloc[x,1]
+    message = df_domoticz.iloc[x,1]
     if x == 0:
         uniqueMessageList.append(message)
     else:
@@ -54,129 +54,141 @@ for x in range(numRows):
             uniqueMessageList.append(message)
             
 
-"""for i in range(len(uniqueMessageList)):
-    print(uniqueMessageList[i])"""
+for i in range(len(uniqueMessageList)):
+    print(uniqueMessageList[i])
 
 #print(len(uniqueMessageList))
 sensorType = list(range(numRows))
 sensorState = list(range(numRows))
-df['Sensor Type'] = sensorType
-df['Sensor State'] = sensorState
+
+
+df_domoticz['Sensor Type'] = sensorType
+df_domoticz['Sensor State'] = sensorState
 count = 0
-successEdit = []
+
 for x in range(numRows):
-    message = df.iloc[x,1]
+    message = df_domoticz.iloc[x,1]
     #print(message)
     #print(type(message))
     if str(message).find('sensor-motion-1 Switch') > -1:
-        df['Sensor Type'][x] = 'Motion Sensor 1'
+        df_domoticz['Sensor Type'][x] = 'Motion Sensor 1'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'  
+            df_domoticz['Sensor State'][x] = 'On'  
     elif str(message).find('sensor-light-1') > -1:
-        df['Sensor Type'][x] = 'Light Sensor 1'
+        df_domoticz['Sensor Type'][x] = 'Light Sensor 1'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'  
+            df_domoticz['Sensor State'][x] = 'On'  
     elif str(message).find('Interior Motion - Room 1') > -1:
-        df['Sensor Type'][x] =  'Motion Sensor Room 1'
+        df_domoticz['Sensor Type'][x] =  'Motion Sensor Room 1'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'  
+            df_domoticz['Sensor State'][x] = 'On'  
     elif str(message).find('Interior Motion - Room 2') > -1:
-        df['Sensor Type'][x] = 'Motion Sensor Room 2'
+        df_domoticz['Sensor Type'][x] = 'Motion Sensor Room 2'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'  
+            df_domoticz['Sensor State'][x] = 'On'  
     elif str(message).find('Smart Plug (AP)') > -1:
-        df['Sensor Type'][x] = 'Smart Plug (AP)'
+        df_domoticz['Sensor Type'][x] = 'Smart Plug (AP)'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'
+            df_domoticz['Sensor State'][x] = 'On'
     elif str(message).find('Lamp') > -1:
-        df['Sensor Type'][x] = 'Lamp'
+        df_domoticz['Sensor Type'][x] = 'Lamp'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'
+            df_domoticz['Sensor State'][x] = 'Off'
         else:
-            df['Sensor State'][x] = 'On'
+            df_domoticz['Sensor State'][x] = 'On'
     elif str(message).find('Lock1') > -1:
-        df['Sensor Type'][x] = 'Lock 1'
+        df_domoticz['Sensor Type'][x] = 'Lock 1'
         count += 1
         if message.lower().find('unlocked') > -1:
-            df['Sensor State'][x] = 'Unlocked'
+            df_domoticz['Sensor State'][x] = 'Unlocked'
         else:
-            df['Sensor State'][x] = 'Locked'
+            df_domoticz['Sensor State'][x] = 'Locked'
     elif str(message).find('Domoticz test message!') > -1:
-        df['Sensor Type'][x] = 'Not applicable'
-        df['Sensor State'][x] = 'Not applicable'
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
         count += 1
     elif str(message).find('Plug (Wink') > -1:
-        df['Sensor Type'][x] = 'Plug (Wink)'
+        df_domoticz['Sensor Type'][x] = 'Plug (Wink)'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'        
+            df_domoticz['Sensor State'][x] = 'Off'        
         else:
-            df['Sensor State'][x] = 'On'
+            df_domoticz['Sensor State'][x] = 'On'
     elif str(message).find('nan') > -1:
-        df['Sensor Type'][x] = 'Not applicable'
-        df['Sensor State'][x] = 'Not applicable'
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
         count += 1                      
     elif str(message).find('Hallway1') > -1:
-        df['Sensor Type'][x] = 'Hallway1 Motion'
+        df_domoticz['Sensor Type'][x] = 'Hallway1 Motion'
         count += 1
         if message.lower().find('off') > -1:
-            df['Sensor State'][x] = 'Off'        
+            df_domoticz['Sensor State'][x] = 'Off'        
         else:
-            df['Sensor State'][x] = 'On'
+            df_domoticz['Sensor State'][x] = 'On'
     elif str(message).find('MotionSensorRoom1') > -1:
-        df['Sensor Type'][x] = 'Motion Sensor Room 1'
+        df_domoticz['Sensor Type'][x] = 'Motion Sensor Room 1'
         count += 1
         if message.lower().find('Motion Detected') > -1:
-            df['Sensor State'][x] = 'On'
+            df_domoticz['Sensor State'][x] = 'On'
             
-
+            ###Need to change this
+    elif str(message).find('Last Update:') > -1:
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
+        count += 1               
+    elif str(message).find('Door') > -1:
+        df_domoticz['Sensor Type'][x] = 'Door'
+        count += 1
+        if message.lower().find('unlocked') > -1:
+            df_domoticz['Sensor State'][x] = 'Unlocked'
+        else:
+            df_domoticz['Sensor State'][x] = 'Locked'
+    elif str(message).find('swx-u-range-sensor-motion-1 Alarm type: 0x07 AC >> OFF') > -1:
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
+        count += 1 
+    elif str(message).find('Alarm Off') > -1:
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
+        count += 1
+    elif str(message).find('Alarm - Interior Motion') > -1:
+        df_domoticz['Sensor Type'][x] = 'Not applicable'
+        df_domoticz['Sensor State'][x] = 'Not applicable'
+        count += 1
+             
 print(count)
+df_domoticz._id = pd.to_datetime(df_domoticz._id)
+#choose certain columns
+df_test = df_domoticz[['_id','_source.message']]
+ #convert _id column to datetime
+df_test._id = pd.to_datetime(df_test._id)
 
-"""swx-u-range-sensor-motion-1 Switch >> OFF (Resolved)
-swx-u-range-sensor-light-1 >> OFF (Resolved)
-swx-u-range-sensor-light-1 >> ON (Resolved)
-swx-u-range-sensor-motion-1 Switch >> ON (Resolved)
-Alarm Off - Interior Motion - Room 1 (Resolved)
-Alarm - Interior Motion - Room 2 (Resolved)
-Alarm - Interior Motion
-Alarm Off
-Alarm - Interior Motion - Room 1 (Resolved)
-nan (Resolved)
-swx-u-range-sensor-motion-1 Alarm type: 0x07 AC >> OFF
-Alarm Off - Interior Motion - Room 2 (Resolved)
-Domoticz test message! (Resolved)
-nan (Resolved)
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-03-19 18:11:22 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-14 10:52:04 [!= 1 minutes]
-Plug (Wink Turned On (Resolved)
-MotionSensorRoom1 - Motion Detected (Resolved)
-Smart Plug (AP) Turned On (Resolved)
-Hallway1 - Motion Detected (Resolved)
-Hallway1 - Alarm Off (Resolved)
-Lock1 Unlocked (Resolved)
-MotionSensorRoom1 - Monitoring
-Lock1 Locked (Resolved)
-Plug (Wink Turned Off (Resolved)
-Lamp Turned On (Resolved)
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-17 18:11:20 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-18 06:51:50 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-18 19:51:31 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-19 07:17:48 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-19 18:11:41 [!= 1 minutes]
-Sensor swx-u-range-sensor-light-1 Last Update: 2018-04-20 06:49:16 [!= 1 minutes]"""
+ 
+ #sort the data in ifttt
+df_domoticz['intrusion'] = [0] * df_domoticz.shape[0] #adds intrusion columns
+df_domoticz['dates'] = [d.date() for d in df_domoticz['_id']] #adds dates column (used for checking date w/o time)
+for x in range(numRows): #for every date:
+    current_date = df_domoticz['dates'][x]
+    if current_date.strftime('%Y-%m-%d') == "2018-04-18" or current_date.strftime('%Y-%m-%d') =="2018-04-13" or current_date.strftime('%Y-%m-%d') =="2018-04-12": #if it's equal to this date intrusion data was recorded
+        df_domoticz['intrusion'][x] = 1 #put a 1 in the intrusion column on the same row
+        
+#df_domoticz = df_domoticz[['_id', 'Sensor Type', 'Sensor State', 'intrusion']] #organize dataframe
+df_domoticz = df_domoticz.sort_values(by=['Sensor Type', '_id']) #sort data
+
+#6/21/18 notes: There appears to be a bug with Motion Sensor- Room 1 data where it does not change the sensor state column
